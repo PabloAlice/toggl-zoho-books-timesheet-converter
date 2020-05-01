e7ea33f35bd6d0374b71ba459810e595c0eac757
Pablo Alice <pabloalice95@gmail.com>
2020-05-01 14:27:39 -0300

    update script to match date format and set Billable as default

--- index.js
+++ index.js
@@ -10,11 +10,17 @@ function reformatTime(val) {
   return moment(val, 'HH:mm:ss').format('HH:mm');
 }
 
+function reformatDate(val) {
+  return moment(val, 'YYYY-MM-DD').format('DD-MMM-yy')
+}
+
 csv
   .fromPath(inputFile, { headers: true })
   .transform(row => {
-    row.Billable = 'Yes' === row.Billable ? 'Billable' : 'Non Billable';
+    row.Billable = 'Billable';
     row['Start time'] = reformatTime(row['Start time']);
+    row['Start date'] = reformatDate(row['Start date']);
+    row['End date'] = reformatDate(row['End date']);
     row['End time'] = reformatTime(row['End time']);
     row.Duration = reformatTime(row.Duration);
     return row;
